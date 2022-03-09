8be02063a76983212d67222ed300f621fc0d106d
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2022-03-09 17:11:18 -0300

    Lessons 67 at 77: belongs_to, has_many, references and others commands

--- RailsUdemyCurso/Modulo02_Crypto/app/controllers/coins_controller.rb
+++ RailsUdemyCurso/Modulo02_Crypto/app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
   # GET /coins or /coins.json
   def index
@@ -59,12 +60,16 @@ class CoinsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+  def set_mining_type_options
+    @mining_type_options = MiningType.all.pluck(:description, :acronym)
+  end
+
     def set_coin
       @coin = Coin.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
